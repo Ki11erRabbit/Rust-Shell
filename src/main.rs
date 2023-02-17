99abf3c72ca92863f7a89945a2f7411c0191c85a
ki11errabbit <ki11errabbit@posteo.net>
2023-02-17 13:55:43 -0700

    added signal-hook crate got the basics of job adding

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,99 @@ use std::env;
 use std::io::{self,Write};
 use std::fs::File;
 use std::os::unix::process::CommandExt;
+use std::sync::Mutex;
+use std::fmt;
+
+pub enum ProccessState {
+    UNDEF,
+    FG,
+    BG,
+    ST
+}
+
+impl fmt::Display for ProccessState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProccessState::UNDEF => write!(f,"UNDEF"),
+            ProccessState::FG => write!(f,"FG"),
+            ProccessState::BG => write!(f,"BG"),
+            ProccessState::ST => write!(f,"ST"),
+        }
+    }
+}
+
+pub struct Job {
+    pid: u32,
+    pgid: u32,
+    jid: u32,
+    state: ProccessState,
+    cmdline: String,
+    pipeline: Mutex<Vec<Child>>
+
+}
+
+impl Job {
+    pub fn new(pid: u32, pgid: u32, jid: u32, state: ProccessState, cmdline: String, pipeline: Mutex<Vec<Child>>) -> Self {
+        Self {pid: pid, pgid: pgid, jid: jid, state: state, cmdline: cmdline, pipeline: pipeline}
+    }
+}
+
+impl fmt::Display for Job {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let result = write!(f,"[{}] ({}) ",self.jid,self.pid);
+        if result == Err(std::fmt::Error) {
+            return result;
+        }
+        let result = match self.state {
+            ProccessState::FG => write!(f,"Foreground "),
+            ProccessState::BG => write!(f,"Running "),
+            ProccessState::ST => write!(f,"Stopped "),
+            ProccessState::UNDEF =>  write!(f,"listjobs: Internal error: job[{}].state={} ",self.jid,self.state),
+        };
+        if result == Err(std::fmt::Error) {
+            return result;
+        }
+        write!(f,"{}",self.cmdline)
+    }
+}
+
+pub struct Jobs {
+    jobs: Vec<Job>,
+    next_jid: u32,
+}
+
+impl Jobs {
+    pub const fn new() -> Self {
+        Self {jobs: Vec::new(),next_jid: 1}
+    }
+
+    pub fn addjob(&mut self, pid: u32, pgid: u32, state: ProccessState, cmdline: String, pipeline: Mutex<Vec<Child>>) {
+       self.jobs.push(Job::new(pid,pgid,self.next_jid,state,cmdline,pipeline)); 
+       self.next_jid += 1;
+    }
+}
+
+impl fmt::Display for Jobs {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for job in self.jobs.iter() {
+            
+            let result = write!(f,"{}",job);
+
+            if result == Err(std::fmt::Error) {
+                return result;
+            }
+        }
+        Ok(())
+    }
+}
+
 
 const PROMPT: &str = "tsh> ";
 static mut VERBOSE:i32 = 0;
+static mut JOBS: Jobs = Jobs::new();
+
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -56,7 +146,7 @@ fn eval(cmdline: String) {
     }
     let argv: Vec<String>;
     let _bg: i32;
-    let pair = parseline(cmdline);
+    let pair = parseline(&cmdline);
     _bg = pair.0;
     argv = pair.1;
     
@@ -119,17 +209,21 @@ fn eval(cmdline: String) {
             group_id = processes[i].id().try_into().unwrap();
         }
     }
-
+    let temp =  group_id.try_into().unwrap();
+    unsafe {
+        JOBS.addjob(temp, temp, ProccessState::FG, cmdline, Mutex::new(processes));
+    }
+/*
     for i in 0..processes.len() {
         match processes[i].wait() {
             Ok(_status) => (),
             Err(error) => eprintln!("{}", error)
         }
-    }
+    }*/
 
 }
 
-fn parseline(cmdline: String) -> (i32,Vec<String>) {
+fn parseline(cmdline: &String) -> (i32,Vec<String>) {
     if unsafe { VERBOSE == 1 } {
         println!("Parseline");
     }
@@ -255,6 +349,12 @@ fn builtin_cmd(argv: &Vec<String>) -> i32 {
     }
     else if argv[0].as_str() == "exit" {
         process::exit(0);
+    }
+    else if argv[0].as_str() == "jobs" {
+        unsafe {print!("{}",JOBS);}
+        io::stdout().flush().unwrap();
+ 
+        return 1;
     }
     
     return 0;
